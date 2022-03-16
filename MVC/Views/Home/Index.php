19db350b4186f5db4adc9478d51a0069fddc38e3
heyiamhy <93415271+heyiamhy@users.noreply.github.com>
2022-03-16 22:22:35 +0700

    BASICAGILE-18 HOME LAYOUT

--- MVC/Views/Home/Index.php
+++ MVC/Views/Home/Index.php
@@ -22,6 +22,166 @@
 			<span id="btn3" onclick="clickSlide(2,false)"></span>
 		</div>
 		<!-- buttons end -->
+		<!-- products start -->
+		<div class="products">
+
+			<!-- view-products start -->
+			<h5 style="padding-top:15px; font-family: 'Big Shoulders Stencil Text', cursive;font-weight:bold">
+				Top View
+				<span></span>
+			</h5>
+
+			<?php foreach ($model['listView'] as $item) : ?>
+				<div class="product-card">
+					<?php if ($item['Discount'] > 0) : ?>
+						<div style="position:absolute;top:-15px;left:0px;width:70px;height:70px" title="SALE <?php echo $item['Discount']; ?>% NOW">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/sale.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if ($item['Quantity'] < 1) : ?>
+						<div style="position:absolute;top:0px;right:0px;width:70px;height:70px" title="SOLD OUT">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/soldout.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if (isset($_SESSION['VISITED_SESSION']) && in_array($item['ID'], $_SESSION['VISITED_SESSION'])) : ?>
+						<label class="visited">Seen <i class="fas fa-check-double"></i></label>
+					<?php endif; ?>
+					<label class="hot"><i class="far fa-eye"> <label><?php echo $item['View']; ?></label></i></label>
+					<div class="image-move move">
+						<img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt="">
+					</div>
+					<div class="image-card">
+						<a onclick="updateView(<?php echo $item['ID'] ?>)" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>"><img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt=""></a>
+					</div>
+					<div class="content-card">
+						<h5><?php echo $item['ProductName']; ?></h5>
+						<?php if ($item['Discount'] > 0) : ?>
+							<h6 style="margin:0">Price: <?php echo number_format($item['Price'] - ($item['Price'] * $item['Discount'] / 100), 0, '', ','); ?> đ</h6>
+							<div style="display:flex;justify-content:center;align-items:center">
+								<small style="text-decoration:line-through"><?php echo number_format($item['Price'], 0, '', ','); ?> đ </small> <small> -<?php echo $item['Discount']; ?>%</small>
+							</div>
+						<?php else : ?>
+							<h6>Price: <?php echo number_format($item['Price'], 0, '', ','); ?> đ</h6>
+						<?php endif; ?>
+						<div class="btn-content-card">
+							<a onclick="updateView(<?php echo $item['ID'] ?>)" class="view-card" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>">View</a>
+							<div class="hover-card">
+								<i class="fas fa-cart-arrow-down"></i>
+								<button onclick="addCart(<?php echo $item['ID']; ?>,1)" style="width: 100%" class="add-cart-card">Add Cart
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+
+			<a href="<?php echo BASE_URL; ?>Product/TopProduct/View" style="text-decoration: none;display:block; "> View more <i class="fas fa-angle-double-right"></i></a>
+			<!-- view-products end -->
+
+			<!-- hot-products start -->
+			<h5 style="padding-top:15px; font-family: 'Big Shoulders Stencil Text', cursive;font-weight:bold">
+				Hot Product
+				<span></span>
+			</h5>
+			<?php foreach ($model['listHot'] as $item) : ?>
+				<div class="product-card">
+					<?php if ($item['Discount'] > 0) : ?>
+						<div style="position:absolute;top:-15px;left:0px;width:70px;height:70px" title="SALE <?php echo $item['Discount']; ?>% NOW">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/sale.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if ($item['Quantity'] < 1) : ?>
+						<div style="position:absolute;top:0px;right:0px;width:70px;height:70px" title="SOLD OUT">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/soldout.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if (isset($_SESSION['VISITED_SESSION']) && in_array($item['ID'], $_SESSION['VISITED_SESSION'])) : ?>
+						<label class="visited">Seen <i class="fas fa-check-double"></i></label>
+					<?php endif; ?>
+					<label class="hot"><i class="far fa-eye"> <label><?php echo $item['View']; ?></label></i></label>
+					<div class="image-move move">
+						<img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt="">
+					</div>
+					<div class="image-card">
+						<a onclick="updateView(<?php echo $item['ID'] ?>)" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>"><img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt=""></a>
+					</div>
+					<div class="content-card">
+						<h5><?php echo $item['ProductName']; ?></h5>
+						<?php if ($item['Discount'] > 0) : ?>
+							<h6 style="margin:0">Price: <?php echo number_format($item['Price'] - ($item['Price'] * $item['Discount'] / 100), 0, '', ','); ?> đ</h6>
+							<div style="display:flex;justify-content:center;align-items:center">
+								<small style="text-decoration:line-through"><?php echo number_format($item['Price'], 0, '', ','); ?> đ </small> <small> -<?php echo $item['Discount']; ?>%</small>
+							</div>
+						<?php else : ?>
+							<h6>Price: <?php echo number_format($item['Price'], 0, '', ','); ?> đ</h6>
+						<?php endif; ?>
+						<div class="btn-content-card">
+							<a onclick="updateView(<?php echo $item['ID'] ?>)" class="view-card" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>">View</a>
+							<div class="hover-card">
+								<i class="fas fa-cart-arrow-down"></i>
+								<button onclick="addCart(<?php echo $item['ID']; ?>,1)" style="width: 100%" class="add-cart-card">Add Cart
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+			<a href="<?php echo BASE_URL; ?>Product/TopProduct/Hot" style="text-decoration: none;display:block;"> View more <i class="fas fa-angle-double-right"></i></a>
+			<!-- hot-products end -->
+
+			<!-- new-products start -->
+			<h5 style="padding-top:15px; font-family: 'Big Shoulders Stencil Text', cursive;font-weight:bold">
+				New Product
+				<span></span>
+			</h5>
+			<?php foreach ($model['listNew'] as $item) : ?>
+				<div class="product-card">
+					<?php if ($item['Discount'] > 0) : ?>
+						<div style="position:absolute;top:-15px;left:0px;width:70px;height:70px" title="SALE <?php echo $item['Discount']; ?>% NOW">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/sale.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if ($item['Quantity'] < 1) : ?>
+						<div style="position:absolute;top:0px;right:0px;width:70px;height:70px" title="SOLD OUT">
+							<img style="width:100%;height:100%" src="<?php echo IMAGE_URL . '/soldout.png'; ?>" alt="">
+						</div>
+					<?php endif; ?>
+					<?php if (isset($_SESSION['VISITED_SESSION']) && in_array($item['ID'], $_SESSION['VISITED_SESSION'])) : ?>
+						<label class="visited">Seen <i class="fas fa-check-double"></i></label>
+					<?php endif; ?>
+					<label class="hot"><i class="far fa-eye"> <label><?php echo $item['View']; ?></label></i></label>
+					<div class="image-move move">
+						<img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt="">
+					</div>
+					<div class="image-card">
+						<a onclick="updateView(<?php echo $item['ID'] ?>)" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>"><img src="<?php echo IMAGE_URL . '/' . $item['Image']; ?>" alt=""></a>
+					</div>
+					<div class="content-card">
+						<h5><?php echo $item['ProductName']; ?></h5>
+						<?php if ($item['Discount'] > 0) : ?>
+							<h6 style="margin:0">Price: <?php echo number_format($item['Price'] - ($item['Price'] * $item['Discount'] / 100), 0, '', ','); ?> đ</h6>
+							<div style="display:flex;justify-content:center;align-items:center">
+								<small style="text-decoration:line-through"><?php echo number_format($item['Price'], 0, '', ','); ?> đ </small> <small> -<?php echo $item['Discount']; ?>%</small>
+							</div>
+						<?php else : ?>
+							<h6>Price: <?php echo number_format($item['Price'], 0, '', ','); ?> đ</h6>
+						<?php endif; ?>
+						<div class="btn-content-card">
+							<a onclick="updateView(<?php echo $item['ID'] ?>)" class="view-card" href="<?php echo BASE_URL . 'Product/Detail/' . $item['ID']; ?>">View</a>
+							<div class="hover-card">
+								<i class="fas fa-cart-arrow-down"></i>
+								<button onclick="addCart(<?php echo $item['ID']; ?>,1)" style="width: 100%" class="add-cart-card">Add Cart
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php endforeach; ?>
+			<a href="<?php echo BASE_URL; ?>Product/TopProduct/New" style="text-decoration: none;display:block;"> View more <i class="fas fa-angle-double-right"></i></a>
+			<!-- new-products end -->
+
+		</div>
+		<!-- products end -->
 		
 	</div>
 </div>
