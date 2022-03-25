cb0ae4db893df5ea1d3e8eedc4c189b05ca32d59
anderson113 <73542040+anderson113@users.noreply.github.com>
2022-03-25 21:24:28 +0700

    fix controller search

--- MVC/Controllers/Home.php
+++ MVC/Controllers/Home.php
@@ -18,17 +18,21 @@ class Home extends ViewModel{
 	}
 	public function Searching(){
 		$productSearchingJSON = '';
-		if (isset($_POST['searchProduct'])){
-			if (isset($_POST['advancedCheck'])){
-				$cate = $_POST['selectBox']=='All'?0:($_POST['selectBox']=='Mobiles'?1:($_POST['selectBox']=='Tablets'?2:($_POST['selectBox']=='Cameras'?3:4)));
-				$priceFrom = str_replace('0.', '0', $_POST['priceFrom']);
-				$priceTo = str_replace('0.', '0', $_POST['priceTo']);
-				$productSearchingJSON = json_decode($this->product->advancedSearch($_POST['searchName'],$cate,$priceFrom,$priceTo),true);
-			}
-			else{
-				$productSearchingJSON = json_decode($this->product->basicSearch($_POST['searchName']),true);
-			}
-		}
+		// if (isset($_POST['searchProduct'])){
+		// 	if (isset($_POST['advancedCheck'])){
+		// 		$cate = $_POST['selectBox']=='All'?0:($_POST['selectBox']=='Mobiles'?1:($_POST['selectBox']=='Tablets'?2:($_POST['selectBox']=='Cameras'?3:4)));
+		// 		$priceFrom = str_replace('0.', '0', $_POST['priceFrom']);
+		// 		$priceTo = str_replace('0.', '0', $_POST['priceTo']);
+		// 		$productSearchingJSON = json_decode($this->product->advancedSearch($_POST['searchName'],$cate,$priceFrom,$priceTo),true);
+		// 	}
+		// 	else{
+		// 		$productSearchingJSON = json_decode($this->product->basicSearch($_POST['searchName']),true);
+		// 	}
+		// }
+		$cate = $_POST['selectBox']=='All'?0:($_POST['selectBox']=='Mobiles'?1:($_POST['selectBox']=='Tablets'?2:($_POST['selectBox']=='Cameras'?3:4)));
+		$priceFrom = str_replace('0.', '0', $_POST['priceFrom']);
+		$priceTo = str_replace('0.', '0', $_POST['priceTo']);
+		$productSearchingJSON = json_decode($this->product->advancedSearch($_POST['searchName'],$cate,$priceFrom,$priceTo),true);
 		$this->loadView('Shared','Layout',[
 			'title'=>'Search',
 			'page'=>'Home/Searching',
